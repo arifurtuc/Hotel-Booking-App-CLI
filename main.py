e743aacc164fddc017c705344a7b50d3d94c5ab1
arif <arifur.tuc@gmail.com>
2023-11-27 12:51:01 +0100

    Implement credit card validation method

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ import pandas
 # Reading hotel information from a CSV file
 df = pandas.read_csv("hotels.csv", dtype={"id": str})
 
+# Reading credit card info from a CSV file
+df_cards = pandas.read_csv("cards.csv", dtype=str).to_dict(orient="records")
+
 
 # Represents a hotel entity with booking and availability functionalities
 class Hotel:
@@ -30,13 +33,13 @@ class Hotel:
 class Reservation:
     def __init__(self, customer_name, hotel_object):
         """Initialize a Reservation object with customer name and the
-            corresponding hotel object"""
+        corresponding hotel object"""
         self.customer_name = customer_name
         self.hotel = hotel_object
 
     def generate(self):
         """Generate and return a confirmation message for the reservation at the
-            selected hotel"""
+        selected hotel"""
         content = f"""
         Thank you for your reservation!
         Here are your booking data:
@@ -46,7 +49,24 @@ class Reservation:
         return content
 
 
-# Displaying the contents of the loaded CSV data
+class CreditCard:
+    def __init__(self, number):
+        """Initialize CreditCard object with the provided card number"""
+        self.number = number
+
+    def validate(self, expiration, cvc, holder):
+        """Validate the credit card based on the provided details."""
+        card_data = {"number": self.number,
+                     "expiration": expiration,
+                     "cvc": cvc,
+                     "holder": holder}
+        if card_data in df_cards:
+            return True
+        else:
+            return False
+
+
+# Displaying the list if hotels
 print(df)
 
 # Asking for user input: hotel ID
@@ -55,15 +75,22 @@ hotel = Hotel(hotel_ID)
 
 # Checking if the selected hotel is available
 if hotel.available():
-    # If available, proceed with the booking process
-    hotel.book()
+    # Simulate credit card validation
+    credit_card = CreditCard(number="1234")
+    if credit_card.validate(expiration="12/26",
+                            cvc="123",
+                            holder="JOHN SMITH"):
+        # Proceed with hotel booking and reservation confirmation
+        hotel.book()
 
-    # Ask for user input: customer name
-    name = input("Enter your name: ")
+        # Ask for user input: customer name
+        name = input("Enter your name: ")
 
-    # Generate a reservation confirmation at the selected hotel
-    reservation_confirmation = Reservation(customer_name=name,
-                                           hotel_object=hotel)
-    print(reservation_confirmation.generate())
+        # Generate a reservation confirmation at the selected hotel
+        reservation_confirmation = Reservation(customer_name=name,
+                                               hotel_object=hotel)
+        print(reservation_confirmation.generate())
+    else:
+        print("There is a problem with your payment!")
 else:
     print("Hotel is not available!")
